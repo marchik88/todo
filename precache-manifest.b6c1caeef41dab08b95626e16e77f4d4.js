6df0c95465f50fdb82bb036c0a764cbd81308117
marchik88 <marchik88@rambler.ru>
2020-07-06 23:26:16 +0300

    Updates

---
+++ precache-manifest.b6c1caeef41dab08b95626e16e77f4d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f83f13ce9938edc5ec0217bfe22ac8",
+    "url": "/marchik88/todo/index.html"
+  },
+  {
+    "revision": "b296ebb5087dd482fbae",
+    "url": "/marchik88/todo/static/css/main.c4d4e56b.chunk.css"
+  },
+  {
+    "revision": "8353ab4e2fa925603a10",
+    "url": "/marchik88/todo/static/js/2.75e6170c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marchik88/todo/static/js/2.75e6170c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b296ebb5087dd482fbae",
+    "url": "/marchik88/todo/static/js/main.1db4db21.chunk.js"
+  },
+  {
+    "revision": "2898cb79aa87524b4a02",
+    "url": "/marchik88/todo/static/js/runtime-main.4e5fc88b.js"
+  }
+]);
